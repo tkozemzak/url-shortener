4407a489e3110e5881a38f5204b36d9d4538d58e
tkozemzak <timkozemzak@gmail.com>
2021-04-13 17:14:58 -0700

    initialize mongodb connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,5 +45,71 @@ export default function Home() {
     }
   };
 
-  return <div className={styles.container}></div>;
+  const onFinishedFailed = () => {
+    setStatus("error");
+    const error = form.getFieldError("link").join(" ");
+    setMessage(error);
+  };
+
+  return (
+    <Layout>
+      <Head>
+        <title>Tim's Url Shortener</title>
+      </Head>
+      <Header>
+        <div className={styles.logo} />
+      </Header>
+      <Content className={styles.content}>
+        <div className={styles.shortener}>
+          <Title level={5}>Copy &amp; Paste your lengthy link</Title>
+          <Form
+            form={form}
+            onFinish={onFinish}
+            onFinishFailed={onFinishedFailed}
+          >
+            <div className={styles.linkField}>
+              <div className={styles.linkFieldInput}>
+                <Form.Item
+                  name="link"
+                  noStyle
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please paste a correct link",
+                      type: "url",
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="https://my-super-long-link.com/blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah"
+                    size="large"
+                  />
+                </Form.Item>
+              </div>
+              <div className={styles.linkFieldButton}>
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    style={{ width: "100%" }}
+                    size="large"
+                  >
+                    Shorten!
+                  </Button>
+                </Form.Item>
+              </div>
+            </div>
+          </Form>
+          {["error", "success"].includes(status) && (
+            <Alert
+              showIcon
+              message={message}
+              type={status as "error" | "success"}
+            />
+          )}
+        </div>
+      </Content>
+      <Footer className={styles.footer}>Tim's Url Shortener</Footer>
+    </Layout>
+  );
 }
